babce40feb44c6cc00fb02abfc9952b65f6c6507
Gavazadd <gavazadd@gmail.com>
2023-07-07 19:42:05 +0300

    add serverless.ts

--- serverless.ts
+++ serverless.ts
@@ -1,38 +1,86 @@
 import type { AWS } from '@serverless/typescript';
-
-import hello from '@functions/hello';
+import {hello,signUp } from './src/functions';
 
 const serverlessConfiguration: AWS = {
-  service: 'aws-serverless-api',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
-  provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
-    apiGateway: {
-      minimumCompressionSize: 1024,
-      shouldStartNameWithService: true,
+    service: 'aws-serverless-api',
+    frameworkVersion: '3',
+    plugins: ['serverless-esbuild'],
+    provider: {
+        name: 'aws',
+        runtime: 'nodejs14.x',
+        apiGateway: {
+            minimumCompressionSize: 1024,
+            shouldStartNameWithService: true,
+        },
+        environment: {
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+            NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+        },
+        iamRoleStatements: [
+            {
+                Effect: 'Allow',
+                Action: [
+                    'dynamodb:*',
+                    'ses:*',
+                    'sqs:*',
+                ],
+                Resource: '*',
+            },
+        ],
     },
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    // import the function via paths
+    functions: { hello, signUp},
+    package: { individually: true },
+    custom: {
+        resources: {
+            Resources: {
+                UsersTable: {
+                    Type: 'AWS::DynamoDB::Table',
+                    Properties: {
+                        TableName: 'Users',
+                        AttributeDefinitions: [
+                            {
+                                AttributeName: 'id',
+                                AttributeType: 'S',
+                            },
+                            {
+                                AttributeName: 'email',
+                                AttributeType: 'S',
+                            },
+                            {
+                                AttributeName: 'password',
+                                AttributeType: 'S',
+                            },
+                        ],
+                        KeySchema: [
+                            {
+                                AttributeName: 'id',
+                                KeyType: 'HASH',
+                            },
+                            {
+                                AttributeName: 'email',
+                                KeyType: 'RANGE',
+                            },
+                        ],
+                        ProvisionedThroughput: {
+                            ReadCapacityUnits: 5,
+                            WriteCapacityUnits: 5,
+                        },
+                    },
+                },
+            },
+        },
+        esbuild: {
+            bundle: true,
+            minify: false,
+            sourcemap: true,
+            exclude: ['aws-sdk'],
+            target: 'node14',
+            define: { 'require.resolve': undefined },
+            platform: 'node',
+            concurrency: 10,
+        },
     },
-  },
-  // import the function via paths
-  functions: { hello },
-  package: { individually: true },
-  custom: {
-    esbuild: {
-      bundle: true,
-      minify: false,
-      sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
-      concurrency: 10,
-    },
-  },
 };
 
 module.exports = serverlessConfiguration;
